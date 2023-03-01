7291bff921c9e66128811fd290123f4db0e4c4bb
olamideare <63550820+olamideare@users.noreply.github.com>
2023-03-01 16:39:09 +0100

    donate link

--- js/flutter.js
+++ js/flutter.js
@@ -2,3 +2,9 @@ const flutterwaveRadio = document.getElementById("flutterwave-radio");
 flutterwaveRadio.addEventListener("click", () => {
   window.open("https://flutterwave.com/donate/fhpydtemfw6o", "_blank");
 });
+
+
+const paystack = document.getElementById("flutterwave-radio");
+flutterwaveRadio.addEventListener("click", () => {
+  window.open("https://flutterwave.com/donate/fhpydtemfw6o", "_blank");
+});
